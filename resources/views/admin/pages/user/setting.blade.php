69d06b2b8335b5601ed52026964ce59906c4c7c9
Produkphp <contoh@Produkphp.com>
2019-02-07 11:00:55 +0700

    Eps-8 & Eps-9

--- resources/views/admin/pages/user/setting.blade.php
+++ resources/views/admin/pages/user/setting.blade.php
@@ -3,6 +3,23 @@
 @section('content')
 <h1>User</h1>
 <hr>
+
+@if(session('result') == 'success')
+<div class="alert alert-success alert-dismissiable fade show">
+	<strong>Update !</strong> Berhasil diupdate.
+	<button type="button" class="close" data-dismiss="alert">
+		&times;
+	</button>
+</div>
+@elseif(session('result') == 'fail')
+<div class="alert alert-danger alert-dismissiable fade show">
+	<strong>Failed !</strong> Gagal diupdate.
+	<button type="button" class="close" data-dismiss="alert">
+		&times;
+	</button>
+</div>
+@endif
+
 <dir class="row">
 	<div class="col-md-6">
 		<form method="post" action="{{ route('admin.user.setting') }}">
@@ -13,26 +30,34 @@
 
 					<div class="form-group form-label-group">
 						<input type="text" name="name"
-						class="form-control"
+						class="form-control {{$errors->has('name')?'is-invalid':''}}"
 						value="{{ old('name',$dt->name) }}"
 						id="iName" placeholder="Name" required>
 						<label for="iName">Name</label>
+						@if($errors->has('name'))
+						<div class="invalid-feedback">{{$errors->first('name')}}</div>
+						@endif
 					</div>
 
 					<div class="form-group form-label-group">
 						<input type="text" name="email"
-						class="form-control"
+						class="form-control {{$errors->has('email')?'is-invalid':''}}"
 						value="{{ old('email',$dt->email) }}"
 						id="iEmail" placeholder="Email" required>
 						<label for="iEmail">Email</label>
+						@if($errors->has('email'))
+						<div class="invalid-feedback">{{$errors->first('email')}}</div>
+						@endif
 					</div>
 
 					<div class="form-group form-label-group">
 						<input type="password" name="password"
-						class="form-control"
-						value="{{ old('password',$dt->password) }}"
+						class="form-control {{$errors->has('password')?'is-invalid':''}}"
 						id="iPassword" placeholder="Password" required>
 						<label for="iPassword">Password</label>
+						@if($errors->has('password'))
+						<div class="invalid-feedback">{{$errors->first('password')}}</div>
+						@endif
 						<div class="form-text text-muted">
 							<small>Kosongkan Password apabila tidak di ubah.</small>
 						</div>
@@ -40,9 +65,12 @@
 
 					<div class="form-group form-label-group">
 						<input type="password" name="repassword"
-						class="form-control"
+						class="form-control {{$errors->has('repassword')?'is-invalid':''}}"
 						id="iRePassword" placeholder="Re Password" required>
 						<label for="iRePassword">Re Password</label>
+						@if($errors->has('repassword'))
+						<div class="invalid-feedback">{{$errors->first('repassword')}}</div>
+						@endif
 					</div>
 
 				</div>
